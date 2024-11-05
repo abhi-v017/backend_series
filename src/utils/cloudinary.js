0e79fe2e26cabad0a9bda9ec685587c17d4f1e31
abhishek verma <abhi.av017@gmail.com>
2024-11-05 20:40:54 +0530

    fileUploader

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs'
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        console.log('file is uploaded on cloundinary', response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
